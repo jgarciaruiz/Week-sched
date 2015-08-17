312468dbf79c67472dff111d69d3d84acabf179a
Javi <github@snaven.com>
2015-08-18 00:18:15 +0200

    minor fixes demo
    dynamic sched "lunes" "15.30"

--- js/main.js
+++ js/main.js
@@ -30,7 +30,38 @@
         console.log(data);
         $('#jsonresp').html($.param(data));
 
+            var timestamp = Math.floor(Date.now() / 1000);
+            //switch day: create appointment in col
+            switch($('#dia').val()) {
+                case "lunes":
+                    coldia = $('.sched-cols').find("[data-column-dia='lunes'] .sched-colwr-content");
+                    item = "<a class='sched-activity cita-lunes "+timestamp+"' href='javascript:void(0)' data-onclick='popup' data-event-id='"+timestamp+"'>"
+                        +"<div class='sched-activity-inner'>"
+                            +"<div class='sched-activity-inner-bar'></div>"
+                            +"<div class='sched-activity-title'>"+$('#titulo').val()+"</div>"
+                            +"<div class='sched-activity-subtitle'>"+$('#comienza').val()+"-"+$('#finaliza').val()+"</div>"
+                            +"<div class='sched-activity-description' data-full='"+$('#descripcion').val()+"'></div>"
+                        +"</div>"
+                    +"</a>";
+                    $(item).appendTo(coldia);
+                    break;
+            }
+
+            //switch time: position & height of the appointment
+            switch($('#comienza').val()) {
+                case "15:30":
+                    $("."+timestamp).css({
+                        "top":"375px",
+                        "height": "124px",
+                        "width": "100%",
+                        "background-color":$('.color-selected').data('color')
+                    });
+                    break;                
+            }
+
+
     });
+
 
 }(jQuery));
 
